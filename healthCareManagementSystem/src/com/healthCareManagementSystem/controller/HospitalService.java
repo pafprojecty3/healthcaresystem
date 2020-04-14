3f20cc0ca3fda2956321799fa22031bf1cb34f9a
janeru <janeru@DESKTOP-802APEU>
2020-04-14 10:47:09 +0530

    update code by adding view function to the doctor schedules

--- healthCareManagementSystem/src/com/healthCareManagementSystem/controller/HospitalService.java
+++ healthCareManagementSystem/src/com/healthCareManagementSystem/controller/HospitalService.java
@@ -14,14 +14,14 @@ import com.healthCareManagementSystem.model.Hospital;
 @Path("/Hospital")
 public class HospitalService {
 	
-		Hospital sheduleObject = new Hospital();
+		Hospital sheduleObj = new Hospital();
 
 		
 		@GET
 		@Path("/")
 		@Produces(MediaType.TEXT_HTML)
 		public String viewAllShedules() {
-			return sheduleObject.viewAllShedules();
+			return sheduleObj.viewAllShedules();
 		}
 	
 	
@@ -31,7 +31,7 @@ public class HospitalService {
 	@Produces(MediaType.TEXT_PLAIN)
 	public String addSchedule(@FormParam("hospitalCode") String hospitalCode , @FormParam("doctorName") String doctorName,
 			@FormParam("roomNo") String roomNo, @FormParam("timeFrom") String timeFrom, @FormParam("timeTo") String timeTo,@FormParam("date") String date ){
-		String output = sheduleObject.addSchedule(hospitalCode, doctorName, roomNo, timeFrom, timeTo, date);
+		String output = sheduleObj.addSchedule(hospitalCode, doctorName, roomNo, timeFrom, timeTo, date);
 		return output;
 	}
 
--- healthCareManagementSystem/src/com/healthCareManagementSystem/model/Hospital.java
+++ healthCareManagementSystem/src/com/healthCareManagementSystem/model/Hospital.java
@@ -13,7 +13,7 @@ public class Hospital {
 	try
 	{
 	Class.forName("com.mysql.jdbc.Driver");
-	connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", "root", "");
+	connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/healthcaremanagment", "root", "");
 	}
 	catch (Exception e)
 	{e.printStackTrace();}
@@ -28,8 +28,8 @@ public class Hospital {
 	if (connection == null)
 	{return "Error while connecting to the database for inserting."; }
 	// create a prepared statement
-	String query = " insert into hospital(itemID,itemCode,itemName,itemPrice,itemDesc)"
-	+ " values (?, ?, ?, ?, ?)";
+	String query = " insert into hospital(hospitalID,hospitalCode,doctorName,roomNo,timeFrom,timeTo,date)"
+	+ " values (?, ?, ?, ?, ? ,? ,?)";
 	PreparedStatement preparedStmt = connection.prepareStatement(query);
 	// binding values
 	preparedStmt.setInt(1, 0);
@@ -67,25 +67,29 @@ public class Hospital {
 	if (connection == null)
 	{return "Error while connecting to the database for reading."; }
 	// Prepare the html table to be displayed
-	output = "<table border=\"1\"><tr><th>Item Code</th><th>Item Name</th><th>ItemPrice</th><th>ItemDescription</th><th>Update</th><th>Remove</th></tr>";
-	String query = "select * from items";
+	output = "<table border=\"1\"><tr><th>Hospital code</th><th>Doctor Name</th><th>Room NO</th><th>Time From</th><th>Time To</th><th>DATE</th><th>Update</th><th>Remove</th></tr>";
+	String query = "select * from hospital";
 				Statement stmt = connection.createStatement();
 	ResultSet rs = stmt.executeQuery(query);
 	// iterate through the rows in the result set
 	while (rs.next())
 	{
-	String itemID = Integer.toString(rs.getInt("itemID"));
-	String itemCode = rs.getString("itemCode");
-	String itemName = rs.getString("itemName");
-	String itemPrice = Double.toString(rs.getDouble("itemPrice"));
-	String itemDesc = rs.getString("itemDesc");
+	String hospitalID = Integer.toString(rs.getInt("hospitalID"));
+	String hospitalCode = rs.getString("hospitalCode");
+	String doctorName = rs.getString("doctorName");
+	String roomNo = rs.getString("roomNo");
+	String timeFrom = rs.getString("timeFrom");
+	String timeTo = rs.getString("timeTo");
+	String date = rs.getString("date");
 	// Add into the html table
-	output += "<tr><td>" + itemCode + "</td>";
-	output += "<td>" + itemName + "</td>";
-	output += "<td>" + itemPrice + "</td>";
-	output += "<td>" + itemDesc + "</td>";
+	output += "<tr><td>" + hospitalCode + "</td>";
+	output += "<td>" + doctorName + "</td>";
+	output += "<td>" + roomNo + "</td>";
+	output += "<td>" + timeFrom + "</td>";
+	output += "<td>" + timeTo + "</td>";
+	output += "<td>" + date + "</td>";
 	// buttons
-	output += "<td><input name=\"btnUpdate\" type=\"button\"value=\"Update\" class=\"btn btn-secondary\"></td>"+ "<td><form method=\"post\" action=\"items.jsp\">"+ "<input name=\"btnRemove\" type=\"submit\" value=\"Remove\"class=\"btn btn-danger\">"+ "<input name=\"itemID\" type=\"hidden\" value=\"" + itemID+ "\">" + "</form></td></tr>";
+	output += "<td><input name=\"btnUpdate\" type=\"button\"value=\"Update\" class=\"btn btn-secondary\"></td>"+ "<td><form method=\"post\" action=\"items.jsp\">"+ "<input name=\"btnRemove\" type=\"submit\" value=\"Cancel\"class=\"btn btn-danger\">"+ "<input name=\"hospitalID\" type=\"hidden\" value=\"" + hospitalID+ "\">" + "</form></td></tr>";
 	}
 	connection.close();
 	// Complete the html table
